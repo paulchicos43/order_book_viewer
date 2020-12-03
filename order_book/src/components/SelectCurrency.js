8c981bf2b5117be405b73928240ade5e0928a852
Paul Chicos <pchicos@wisc.edu>
2020-12-02 18:03:58 -0600

    Style

--- order_book/src/components/SelectCurrency.js
+++ order_book/src/components/SelectCurrency.js
@@ -8,36 +8,59 @@ import '../css/SelectCurrency.css';
 export default function SelectCurrency(props) {
     const [selectedETH, setSelectedETH] = useState(true);
     const [selectedBTC, setSelectedBTC] = useState(false);
+    const [selectedXRP, setSelectedXRP] = useState(false);
 
     /**
-     * Handles ETH radio button. Changes button checks and propogates changes.
+     * Handles ETH radio button.
      */
     const handleETHClick = () => {
         if(!selectedETH) {
             setSelectedETH(true);
             setSelectedBTC(false);
+            setSelectedXRP(false);
             props.setCurrency("ETH-USD");
         }
     }
 
     /**
-     * Handles BTC radio button. Changes button checks and propogates changes.
+     * Handles BTC radio button.
      */
     const handleBTCClick = () => {
         if(!selectedBTC) {
             setSelectedBTC(true);
             setSelectedETH(false);
+            setSelectedXRP(false);
             props.setCurrency("BTC-USD");
+        }
+    }
+
+    /**
+     * Handles XRP radio button.
+     */
+    const handleXRPClick = () => {
+        if(!selectedXRP) {
+            setSelectedBTC(false);
+            setSelectedETH(false);
+            setSelectedXRP(true);
+            props.setCurrency("XRP-USD");
         }
     }
 
 
     return (
         <div className = "RadioText">
-            <input onChange = {() => {}} onClick = { handleETHClick } checked = { selectedETH } type = "radio" id = "ETH-USD" name = "Currency" value = "ETH-USD" />
-            <label className = "RadioLabel" htmlFor = "ETH-USD">ETH/USD</label>
-            <input onChange = {() => {}} onClick = { handleBTCClick } checked = { selectedBTC } type = "radio" id = "BTC-USD" name = "Currency" value = "BTC-USD" />
-            <label htmlFor = "BTC-USD">BTC/USD</label>
+            <div className = "RadioButton">
+                <input onChange = {() => {}} onClick = { handleETHClick } checked = { selectedETH } type = "radio" id = "ETH-USD" name = "Currency" value = "ETH-USD" />
+                <label className = "RadioLabel" htmlFor = "ETH-USD">ETH/USD</label>
+            </div>
+            <div className = "RadioButton">
+                <input onChange = {() => {}} onClick = { handleBTCClick } checked = { selectedBTC } type = "radio" id = "BTC-USD" name = "Currency" value = "BTC-USD" />
+                <label htmlFor = "BTC-USD">BTC/USD</label>
+            </div>
+            <div className = "RadioButton">
+                <input onChange = {() => {}} onClick = { handleXRPClick } checked = { selectedXRP } type = "radio" id = "XRP-USD" name = "Currency" value = "XRP-USD" />
+                <label htmlFor = "XRP-USD">XRP/USD</label>
+            </div>
         </div>
     );
 }
